d939974d7e1286ca29f93e1aae02e980ac350b73
Tanner Babcock <babkock@gmail.com>
2019-08-06 04:43:53 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -19,6 +19,26 @@ if [ -z $1 ]; then
 		popd > /dev/null
 	done
 else
-
+	for i in "${programs[@]}"; do
+		if [ $i == $1 ]; then
+			pushd "$i" > /dev/null
+			printf "\e[93;1mBuilding %s\n" "$i"
+			cargo build --release
+			if [ $? == 0 ]; then
+				if [ $i == "cat" ]; then
+					cp "target/release/main" ../build/cat
+				else
+					cp "target/release/$i" ../build
+				fi
+			else
+				popd > /dev/null
+				printf "\e[91;1m%s failed to build\n" "$i" > /dev/stderr
+				exit 1
+			fi
+			popd > /dev/null
+			exit 0
+		fi
+	done
 fi
+
 
