08d937f47e7ea863364bcd4314fe03a47e01fa63
Tanner Babcock <babkock@gmail.com>
2019-08-14 01:34:54 -0500

    Tests for ls

--- ls/src/tests.rs
+++ ls/src/tests.rs
@@ -12,8 +12,8 @@ use super::*;
 use std::io;
 use std::fs::Metadata;
 use crate::Options;
-use crate::display::{display_permissions, display_permissions_unix, display_file_name, should_display, display_uname, display_item_long};
-use crate::file::{get_metadata, display_dir_entry_size, get_inode, get_file_name};
+use crate::display::{display_permissions, display_file_name, display_file_size, display_uname, display_file_type, display_date};
+use crate::file::get_metadata;
 
 #[test]
 #[cfg(unix)]
@@ -49,7 +49,7 @@ fn t_permissions() {
 
     // the permissions string for /home ought to be...
     assert_eq!(display_permissions(&m), "rwxr-xr-x");
-
+    assert_eq!(display_file_type(m.file_type()), "d");
     // and it's owned by root
     assert_eq!(display_uname(&m, &o), "root");
 
@@ -62,6 +62,7 @@ fn t_permissions() {
     };
 
     assert_eq!(display_permissions(&m), "rw-r--r--");
+    assert_eq!(display_file_type(m.file_type()), "-");
     assert_eq!(display_uname(&m, &o), "root");
 }
 
@@ -69,5 +70,129 @@ fn t_permissions() {
 #[cfg(not(unix))]
 fn t_permissions() {
     assert_eq!((3+2), 5);
+}
+
+// Note: if Cargo.toml is changed, the constant file size in this test will need to be changed
+#[test]
+fn t_file_size() {
+    let o: Options = Options {
+        dirs: vec![String::from(".")],
+        show_hidden: false,
+        ignore_implied: false,
+        dirs_themselves: false,
+        long_listing: false,
+        dereference: false,
+        reverse: false,
+        recurse: false,
+        sort_by_mtime: false,
+        sort_by_ctime: false,
+        sort_by_size: false,
+        no_sort: true,
+        ignore_backups: true,
+        numeric_ids: false,
+        one_file_per_line: false,
+        human_readable: false,
+        classify: false,
+        inode: false,
+        color: false
+    };
+
+    let m = match get_metadata(&PathBuf::from(r"./Cargo.toml"), &o) {
+        Err(_e) => {
+            match get_metadata(&PathBuf::from(r"../Cargo.toml"), &o) {
+                Err(e) => {
+                    panic!("{}", e);
+                },
+                Ok(m) => m
+            }
+        },
+        Ok(m) => m
+    };
+
+    let size = display_file_size(&m, &o);
+    
+    assert_eq!(size, "298");
+}
+
+#[test]
+fn t_dir_size() {
+    let o: Options = Options {
+        dirs: vec![String::from(".")],
+        show_hidden: false,
+        ignore_implied: false,
+        dirs_themselves: false,
+        long_listing: false,
+        dereference: false,
+        reverse: false,
+        recurse: false,
+        sort_by_mtime: false,
+        sort_by_ctime: false,
+        sort_by_size: false,
+        no_sort: true,
+        ignore_backups: true,
+        numeric_ids: false,
+        one_file_per_line: false,
+        human_readable: false,
+        classify: false,
+        inode: false,
+        color: false
+    };
+
+    let m = match get_metadata(&PathBuf::from("./bin"), &o) {
+        Err(_e) => {
+            match get_metadata(&PathBuf::from("./src"), &o) {
+                Err(e) => {
+                    panic!("{}", e);
+                },
+                Ok(m) => m
+            }
+        },
+        Ok(m) => m
+    };
+
+    // ls always shows file size for directories as 4096
+    let size = display_file_size(&m, &o);
+
+    assert_eq!(size, "4096");
+}
+
+// as long as Cargo.toml was last modified in 2019 this will work
+#[test]
+fn t_last_modified() {
+    let o: Options = Options {
+        dirs: vec![String::from(".")],
+        show_hidden: false,
+        ignore_implied: false,
+        dirs_themselves: false,
+        long_listing: false,
+        dereference: false,
+        reverse: false,
+        recurse: false,
+        sort_by_mtime: true,
+        sort_by_ctime: false,
+        sort_by_size: false,
+        no_sort: false,
+        ignore_backups: true,
+        numeric_ids: false,
+        one_file_per_line: false,
+        human_readable: false,
+        classify: false,
+        inode: false,
+        color: false
+    };
+
+    let m = match get_metadata(&PathBuf::from("./Cargo.toml"), &o) {
+        Err(_e) => {
+            match get_metadata(&PathBuf::from("../Cargo.toml"), &o) {
+                Err(e) => {
+                    panic!("{}", e);
+                },
+                Ok(m) => m
+            }
+        },
+        Ok(m) => m
+    };
+
+    assert!(display_date(&m, &o).contains("2019-"));
 }
 
